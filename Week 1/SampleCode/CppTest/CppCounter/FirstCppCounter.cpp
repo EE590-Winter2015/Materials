95561e7f38f4615025bfeb35b22aef1f7124d0fa
Elliot Saba <staticfloat@gmail.com>
2015-01-09 13:41:53 -0800

    Add example code cleaned up from class

---
+++ Week 1/SampleCode/CppTest/CppCounter/FirstCppCounter.cpp
@@ -0,0 +1,19 @@
+﻿// Class1.cpp
+#include "pch.h"
+#include "FirstCppCounter.h"
+
+using namespace CppCounter;
+using namespace Platform;
+
+// Implement the constructor, to do initialization of object state
+FirstCppCounter::FirstCppCounter()
+{
+	this->total = 0;
+}
+
+// Implement incremenyBy, to have the behavior we want it to.
+int FirstCppCounter::incrementBy(int amount)
+{
+	this->total += amount;
+	return this->total;
+}
---
+++ Week 1/SampleCode/CppTest/CppCounter/FirstCppCounter.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+
+namespace CppCounter
+{
+    public ref class FirstCppCounter sealed
+    {
+	// Create a private section in this class declaration to store member variables
+	private:
+		int total;
+
+	// Create a public section in this class declaration to store public API for this counter
+    public:
+		// Constructor, initializes object state (e.g. initialize total to zero)
+		FirstCppCounter();
+
+		// Increment internal counter by amount, then return total amount
+		int incrementBy(int amount);
+    };
+}
