526c02a2d944a71eb35f0a37d2af9bcd533ab4c2
Elliot Saba <staticfloat@gmail.com>
2015-01-22 10:51:37 -0800

    Add Threading test

---
+++ Week 3/SampleCode/Threading/CppThreadingComponent/CppCounter.cpp
@@ -0,0 +1,38 @@
+﻿// Class1.cpp
+#include "pch.h"
+#include "CppCounter.h"
+#include <windows.h>
+
+using namespace CppThreadingComponent;
+using namespace Platform;
+using namespace Windows::System::Threading;
+
+CppCounter::CppCounter()
+{
+	// First, initialize count to zero
+	this->count = 0;
+
+	// Next, create a thread that increments a counter and calls our onTimerTick function ten times a second
+
+	// We do this by first creating a lambda that is what gets run inside the thread
+	auto lambda = [this](IAsyncAction^ operation){
+		// Run until the thread is canceled
+		while (this->threadHandle->Status != AsyncStatus::Canceled) {
+			// Sleep 1/10th of a second.  Don't worry about this function call, or ask me during office hours.
+			WaitForSingleObjectEx(GetCurrentThread(), 100, FALSE);
+
+			// Increment
+			this->count++;
+
+			// Invoke our event
+			this->onTimerTick(this->count);
+		}
+	};
+
+	// Now take that lambda, wrap it in a WorkItemHaandler object as ThreadPool::RunAsync expects, and watch it go!
+	this->threadHandle = ThreadPool::RunAsync(ref new WorkItemHandler(lambda));
+}
+
+CppCounter::~CppCounter() {
+	this->threadHandle->Cancel();
+}
---
+++ Week 3/SampleCode/Threading/Threading/MainPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using CppThreadingComponent;
+using Windows.UI.Core;
+
+namespace Threading
+{
+    public sealed partial class MainPage : Page
+    {
+        CppCounter counter;
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            this.NavigationCacheMode = NavigationCacheMode.Required;
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            // Initialize our counter
+            counter = new CppCounter();
+
+            // Subscribe to its events
+            counter.onTimerTick += counter_onTimerTick;
+        }
+
+        void counter_onTimerTick(int count)
+        {
+            // Let's update a XAML element.  Remember, we HAVE to use the dispatcher here!
+            Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+            {
+                this.textOutput.Text = "Count: " + count;
+            });
+        }
+    }
+}
