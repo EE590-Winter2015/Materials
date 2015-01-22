30fb035045b2a1ffffc731f0616194f1d5f1afb3
Elliot Saba <staticfloat@gmail.com>
2015-01-21 17:27:45 -0800

    Add Interop example code

---
+++ Week 3/SampleCode/Interop/InteropCppComponent/SignalGenerator.h
@@ -0,0 +1,13 @@
+﻿#pragma once
+
+
+namespace InteropCppComponent
+{
+    public ref class SignalGenerator sealed
+    {
+    public:
+        SignalGenerator();
+
+		Platform::Array<float>^ makeSinWave(int N, float freq, float startingPhase);
+    };
+}
