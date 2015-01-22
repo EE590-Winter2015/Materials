34825dd7b6841fbb470b048772c84506dcce48bc
Elliot Saba <staticfloat@gmail.com>
2015-01-22 15:29:48 -0800

    Add Week 3 slides, WASAPI sample code

---
+++ Week 3/SampleCode/WASAPI/libsound/audiotool.h
@@ -0,0 +1,47 @@
+﻿#pragma once
+
+/*
+    AudioTool is a utility class designed to make dealing with audio data easier
+    It provides simple buffer manipulations to array objects for things such as
+    converting from mono -> stero, generation of sounds, and simple calculations
+
+    This class can serve as a good learning tool for how to pass around and deal
+    with the Platform::Array<float> construct.  Feel free to add new functions
+    to serve your purposes in homeworks and projects.
+*/
+
+namespace libsound
+{
+    public ref class AudioTool sealed
+    {
+    public:
+		// Initialize AudioTool with the parameters of the output audio format
+        AudioTool( unsigned int numChannels, unsigned int samplerate );
+		virtual ~AudioTool();
+
+
+		/////////////// SIMPLE BUFFER MANIPULATIONS ///////////////
+		// This converts a source buffer into something the output can handle (e.g. performs upmixing or downmixing if srcNumChannels != this->numChannels)
+		Platform::Array<float>^ convertChannels( const Platform::Array<float>^ data, unsigned int srcNumChannels );
+
+		// Finds the energy in a Platform::Array<float>^
+        // Note: performs the analysis along each channel independently, returning an array of lenth numChannels
+		Platform::Array<float>^ calcStandardDeviation( const Platform::Array<float>^ data );
+
+
+
+		/////////////// SOUND GENERATION ROUTINES ///////////////
+		// Generate random noise using standard library
+		Platform::Array<float>^ randn( unsigned int numSamples );
+
+		// Generate a sin wave that lasts [numSamples] and oscillates [numOscilllations] times
+		Platform::Array<float>^ sin( unsigned int numSamples, float freq );
+		Platform::Array<float>^ sin( unsigned int numSamples, float freq, float startingPhase );
+
+		// Returns [0,0,0,0,.....,0]
+		Platform::Array<float>^ silence( unsigned int numSamples );
+	private:
+		// these are the output audio parameters, which allow us to generate the proper types of audio
+		unsigned int numChannels, samplerate;
+    };
+}
---
+++ Week 3/SampleCode/WASAPI/libsound/soundio.h
@@ -0,0 +1,78 @@
+﻿#pragma once
+
+#include <audioclient.h>
+#include <phoneaudioclient.h>
+
+/*
+    This class presents a more complete (and useful) API over the sound hardware
+    in WP8.  The biggest difference between this class and the API developed in
+    Homework 2 is that now we understand how to use delegates, events, etc... we
+    are going to be using those to expose events to C# and C++/CX code.
+*/
+
+using namespace Windows::Foundation;
+
+namespace libsound
+{
+	// Callback definitions
+	public delegate void AudioInCallback(const Platform::Array<float>^ data);
+	public delegate Platform::Array<float>^ AudioOutCallback( unsigned int numSamples );
+
+
+    public ref class SoundIO sealed
+    {
+    public:
+		// Opens up the audio devices and starts threads for input/output
+		SoundIO();
+		virtual ~SoundIO();
+
+		// Starts callback threads, etc...
+		void start();
+
+		// Stops the callback threads, etc...
+		void stop();
+
+		// Methods used to query the audio output format properties
+		unsigned int getOutputBufferSize();
+		unsigned int getOutputSampleRate();
+		unsigned int getOutputNumChannels();
+		unsigned int getOutputBitdepth();
+
+		// Methods used to query the audio input format properties
+		unsigned int getInputBufferSize();
+		unsigned int getInputSampleRate();
+		unsigned int getInputNumChannels();
+		unsigned int getInputBitdepth();
+
+		// Feed a chunk of audio out to the device.  Returns number of samples written
+		unsigned int writeAudio( const Platform::Array<float>^ data );
+
+		// Read in a chunk of audio from the microphone, returning it as a C# array
+		Platform::Array<float>^ readAudio( void );
+
+		// Event interface! Yeah!
+		event AudioInCallback^ audioInEvent;
+		event AudioOutCallback^ audioOutEvent;
+	private:
+		// Audio devices and clients
+		IAudioClient2 *outputDevice, *inputDevice;
+		IAudioRenderClient *outputClient;
+		IAudioCaptureClient *inputClient;
+
+		// Formats of audio streams for capture and playback
+		WAVEFORMATEX *outputFormat, *inputFormat;
+
+		// Buffer lengths for input and output in samples
+		unsigned int outputBufferLen, inputBufferLen;
+
+		// Threads that get audio input and call their callback, or call their callbacks and output to the sound card
+		void audioInThread( IAsyncAction^ operation );
+		void audioOutThread( IAsyncAction^ operation );
+
+		// Handles to the threads so we can close them down when we die
+		IAsyncAction ^audioInThreadHandle, ^audioOutThreadHandle;
+
+		// Handles that are used to tell if an audio buffer is ready to be read or outputted to
+		HANDLE audioInReady, audioOutReady;
+    };
+}
